cd5df5c96263f9a791fe38082b8bac721debf6bf
Huzaifa Khan <huzaifakaimkhani1@gmail.com>
2022-11-18 16:12:14 +0500

    we are learning different references

--- js/closures.js
+++ js/closures.js
@@ -1,11 +1,56 @@
-function outer() {
-    var b = 10;
-    function inner() {
+// function outer() {
+//     var b = 10;
+//     function inner() {
          
-          var a = 20; 
-          console.log(a+b);
-     }
-    return inner;
- }
+//           var a = 20; 
+//           console.log(a+b);
+//      }
+//     return inner;
+//  }
  
- console.log(outer()());
+//  console.log(outer()());
+
+// let date = new Date();
+// let day = date.getDay();
+// // let  days = ["Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"];
+// let hours = date.getHours();
+// let minutes = date.getMinutes();
+// let seconds = date.getSeconds();
+// let prepand = (hours > 12) ?  "PM" : "AM";
+// hours = (hours >=12)? hours - 12 : hours ;
+
+// if(hours === 0) {
+//     hours = 12;
+//     // prepand = noon;
+// }
+
+// console.log("Current Time : " + hours + ":" + minutes + ":"+seconds+ " "+prepand);
+
+
+// let print_current_page = () =>{
+//     window.print();
+// }
+
+
+// let date = new Date();
+// let day = date.getDay();
+// let month = date.getMonth();
+// let year = date.getFullYear();
+
+//     console.log();
+
+// let side1 = 5;
+// let side2 = 6;
+// let side3 = 7;
+
+// let s = (side1 + side2 +side3)/2;
+
+// let area = Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+// console.log(area);
+
+// let str = "w3resource";
+
+// let rev = str.split('').reverse().join();
+
+
+// console.log(rev);
--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,107 @@
-function sum (a,b){
- console.log(a+b);
-}
-sum(7,8);
-
-function con(){
-    let a="Huzaifa";
-    console.log(a);
-}
-
-con();
-
-
-// f();
-// var f = function (){
-//     console.log("ok");
+// function sum (a,b){
+//  console.log(a+b);
 // }
+// sum(7,8);
+
+
+// con();
+// function con(){
+//     let a="Huzaifa";
+//     console.log(a);
+// }
+
+//     return console.log("ok");
+
+// }
+// f();
+// var f = () =>  {
+// f();
+
+
+
+
+
+
+
+
+
+
+
+// console.log(f);
+// var f = function (){       
+// return console.log("ok");
+// }
+    
+// const arr = [5,6,7,8,9];
+
+// function double(x){
+//     return x*2;
+// }
+// const output = arr.map(double);
+// console.log(output);
+
+
+// function _map(a,f){
+//     var b = [];
+//     for(let i = 0; i < a.length; i++){
+//         b.push(f(a[i]));
+//     }
+//     return b;
+// }
+
+
+// console.log(_map(arr,double));
+
+
+// obj = {
+//     a:[1,2,3]
+// }
+
+
+// pass by value
+
+// a  = 5;
+// b = a;
+// b = 10;
+
+// console.log(a);
+
+// pass by reference
+
+let obj = {
+    a:1,
+    b:2,
+    c:{
+        n:6,
+        m:7
+    }
+}
+
+
+// console.log(obj.b) 
+
+// obj2 = obj;
+
+// obj.b=5
+
+
+
+
+
+
+// console.log(obj.b) 
+
+// console.log(obj2.b)
+
+let obj2 = {...obj};
+console.log(obj.c.n);
+obj2.b=15;
+obj2.c.n=20
+z = obj.c
+z.n=25
+console.log(obj.c.n);
+console.log(z);
+// console.log(obj.b);
+// console.log(obj.c);
+
+
